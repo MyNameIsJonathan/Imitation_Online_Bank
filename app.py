a189c96563935b869be27cc9a0173ab6a22bc3a7
Jonathan <jonathanholson@gmail.com>
2019-07-08 19:18:58 -0700

    Working connection between flask, mongo

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
+# Create flask application
 app = Flask(__name__)
+
+# Configure flask app with link to mongodb using flask-pymongo. 
+app.config["MONGO_URI"] = "mongodb://mongodb:27017/test"
+
+# Expose database as the db attribute.
+mongo = PyMongo(app)
+
+
 
 @app.route("/")
 def hello():
-    return "Hello World 3!"
+    my_person = mongo.db.people.find_one_or_404()
+    return my_person['first']
